f393949d05aee4565e9b1d81c7eac8df9f199d1f
woodchucker <ditrafabri@gmail.com>
2020-11-20 09:05:28 +0100

    0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  ckValue = '';
+  isChecked = true;
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  food = 'pizza-1';
+  onCheckBoxChange($e: MatCheckboxChange): void {
+    console.log(($e));
+  }
+  onInputChange($e: any,errors:any):void{
+    console.log(errors);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
